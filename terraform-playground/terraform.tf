9ef5d5572a1f439c0423e1dee2f3455e77f72c7f
hemantic <artem.kiselev@gmail.com>
2020-11-14 00:56:28 +0300

    ...

--- terraform-playground/terraform.tf
+++ terraform-playground/terraform.tf
@@ -14,7 +14,6 @@ terraform {
   backend "s3" {
     bucket  = "hemantic-playground-terraform-state"
     key     = "global/s3/terraform.tfstate"
-    region  = "eu-north-1"
     encrypt = true
   }
 }
